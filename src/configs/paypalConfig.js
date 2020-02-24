0a7b31872f3058c6194681efa2ad10bbbdb3d69f
Zack Yin <youcancallmeallen@gmail.com>
2020-02-24 18:42:54 -0500

    Update paypalConfig.js

--- src/configs/paypalConfig.js
+++ src/configs/paypalConfig.js
@@ -1,4 +1,6 @@
+// Paypal sandbox api (https://developer.paypal.com/docs/api/overview/)
+// replace empty string with your 'Client ID' and 'Secret'
 export default {
-  username: 'AWijdE5xWwu7gyt-rkEjgvucwKrC3vTFhwY4L-FMXZAQSKJzQdJY-d0I51JJZxwFag6mCcDhTVEX-WYk',
-  password: 'EAWr7rG9XVeyK2RLLvg1tdrOzqQj1RaEVI6jIWNkrDpTTUKXfOUBtZFSDNJ-DZ_uOCQts_Kt_GqO8Xn9'
+  username: '',
+  password: ''
 }
